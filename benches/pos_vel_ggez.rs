dd8bb8148b8699e2051ed5309c6b5f30335cb3b0
Simon Heath <icefoxen@gmail.com>
2017-04-24 19:22:30 -0400

    Build+update bench works

---
+++ benches/pos_vel_ggez.rs
@@ -0,0 +1,48 @@
+#![feature(test)]
+
+extern crate test;
+use test::Bencher;
+
+extern crate ggez_goodies;
+use ggez_goodies::ecs::*;
+
+extern crate ecs_bench;
+
+use ecs_bench::pos_vel::{Position, Velocity, N_POS_VEL, N_POS};
+
+#[derive(Debug, Copy, Clone)]
+struct PosComp(Position);
+#[derive(Debug, Copy, Clone)]
+struct VelComp(Velocity);
+
+fn build() -> World<u32> {
+    let mut w = World::new();
+    {
+
+        for e in 0..N_POS_VEL {
+            w.create_entity(PosComp(Position { x: 0.0, y: 0.0 }), VelComp(Velocity { dx: 0.0, dy: 0.0 }));
+        }
+        for e in N_POS_VEL..(N_POS_VEL + N_POS) {
+            w.create_entity(PosComp(Position { x: 0.0, y: 0.0 }), ());
+        }
+    }
+    w
+}
+
+#[bench]
+fn bench_build(b: &mut Bencher) {
+    b.iter(|| build());
+}
+
+#[bench]
+fn bench_update(b: &mut Bencher) {
+    let mut world = build();
+
+    b.iter(|| {
+        world.run2(|p: &PosComp, v: &VelComp, events: _, writer: _| {
+            let p = Position { x: p.0.x + v.0.dx, y: p.0.y + v.0.dy};
+            (PosComp(p), *v)
+        });
+        world.finish();
+    });
+}
